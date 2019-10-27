f85898f973f53e4ed847f12c2c0338c3a1d3359b
esra <esra.zihni@bccn-berlin.de>
2019-10-27 11:43:27 +0000

    integrate subsampling.py into helper_functions.py

--- code/utils/helper_functions.py
+++ code/utils/helper_functions.py
@@ -1,11 +1,24 @@
 import numpy as np
-import pickle
-import os
-
 import pandas as pd
 
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, recall_score
 
+
+def subsample(X,y,subsampling_type):
+    df = pd.concat([X,y],axis=1)
+    label = list(df)[-1]
+
+    if subsampling_type=='random':
+        df_bad = df[(df[label] == 1)]
+        df_good = df[(df[label] == 0)]
+
+        df_sub = pd.concat([df_good.sample(len(df_bad.index)),df_bad])
+        X_new, y_new = df_sub.drop(label,axis=1), df_sub[[label]]
+
+    elif subsampling_type=='none':
+        X_new, y_new = X,y
+
+    return X_new, y_new
 
 def predict_probability(data,model,model_name):
         if model_name == 'MLP':
